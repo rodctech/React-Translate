26bdc4c98e47e0a388354401b5c7ff326f7a301e
Rodrigo Chavez <rodctech@gmail.com>
2019-05-29 11:16:50 -0500

    Start of App 298- Basic Language Selector done in App Component

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({language})
+    };
+
+    render() {
+        return (
+            <div className={"ui container"}>
+                <div>
+                    Select language:
+                    <i className={"flag us"}
+                       onClick={() => this.onLanguageChange('english')}
+                    />
+                    <i className={"flag nl"}
+                       onClick={() => this.onLanguageChange('dutch')}
+                    />
+                </div>
+                {this.state.language}
+            </div>
+        );
+    }
+}
+
+export default App;
