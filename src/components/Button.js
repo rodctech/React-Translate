2649b0286084a82be783e53242352f6b0207be5d
Rodrigo Chavez <rodctech@gmail.com>
2019-05-30 12:29:34 -0500

    305- Context Consumer, accessing data with Consumer approach.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,23 @@ import React, {Component} from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
-    static contextType = LanguageContext;
+    //static contextType = LanguageContext;  PROVIDER APPROACH
+    // ^^Not needed when using a Consumer approach instead of contextType
+
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Voorleggen';
+    }
 
     render() {
         //console.log(this.context);
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+       // old PROVIDER  const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
 
-        return (
-            <button className={"ui button primary"}>{text}</button>
+        return ( /// replaced with <LangCont.Consumer> {text}
+            <button className={"ui button primary"}>
+               <LanguageContext.Consumer>
+                   {(value) => this.renderSubmit(value)}
+               </LanguageContext.Consumer>
+            </button>
         );
     }
 }
