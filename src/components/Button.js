4b76589b6bdd85c292c75fce60278acfc1f3ca4e
Rodrigo Chavez <rodctech@gmail.com>
2019-05-30 15:08:07 -0500

    312- Finished Context Sys instead of Redux

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,15 +7,15 @@ class Button extends Component {
     //static contextType = LanguageContext;  PROVIDER APPROACH
     // ^^Not needed when using a Consumer approach instead of contextType
 
-    renderSubmit(value) {
-        return value === 'english' ? 'Submit' : 'Voorleggen';
+    renderSubmit(language) {
+        return language === 'english' ? 'Submit' : 'Voorleggen';
     }
 
     renderButton(color) {
         return(
             <button className={`ui button ${color}`}>
                 <LanguageContext.Consumer>
-                    {(value) => this.renderSubmit(value)}
+                    {({language}) => this.renderSubmit(language)}
                 </LanguageContext.Consumer>
             </button>
         );
